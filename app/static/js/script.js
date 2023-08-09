e59dee4b8ffd6ff26d4bda0599a8e0a213550077
mo <wudiwangzun@163.com>
2023-08-08 18:38:47 -0700

    nginx config static

---
+++ app/static/js/script.js
@@ -0,0 +1,9 @@
+
+// Example JavaScript code
+document.addEventListener("DOMContentLoaded", function() {
+  const heading = document.querySelector("h1");
+  heading.addEventListener("click", function() {
+    alert("You clicked the heading!");
+  });
+});
+
